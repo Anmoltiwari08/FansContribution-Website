883247bb1cad95384ef46e60759037c2204fdbd5
Anmol <80anmoltiwari08@gmail.com>
2024-09-27 19:00:39 +0530

    change in head

--- src/api/Payment_api.js
+++ src/api/Payment_api.js
@@ -4,8 +4,26 @@
  * node modules
  */
 
-const axios = require('axios').default ;
+const axios = require('axios').default;
 const crypto = require('crypto')
 
 require('dotenv').config();
 
+const cryptomus = axios.create({
+    baseURL: 'https://api.crytomus.com/vi'
+})
+
+const DEFAULT_CURRENCY = 'USD';
+
+const createInvoice = async (amount)=>{
+
+    try {
+   
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+
+}
+
+module.exports = { createInvoice }
